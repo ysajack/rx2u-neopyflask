4dbec6a255996c9cf3e08e9178bc61b33f0c2978
Jackman Ysa <jackhman.ysa@kp.org>
2018-05-05 14:06:01 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,194 @@
+from flask import Flask, render_template, url_for, request, redirect, session, flash
+from models import User, Pharmacy, Uber
+import sys
+import json
+
+app = Flask(__name__)
+app.secret_key = 'verytoplevelsecret'
+
+@app.route('/')
+def home():
+    session.pop('username', None)
+    return render_template('home.html')
+
+@app.route('/requestpickup', methods=['GET','POST'])
+def requestpickup(userinfo=None):
+    if request.method == 'POST':
+        if 'username' in session:
+            userinfo = User.populateUserInfo(session["username"])
+            return render_template('requestpickup.html', userinfo=userinfo)
+    else:
+        if 'username' in session:
+            userinfo = list(User.populateUserInfo(session["username"]))
+            return render_template('requestpickup.html', userinfo=userinfo)
+        else:
+            data = {"first": 'test',
+                "last": None,
+                "phone": None,
+                "dob": None,
+                "address": None,
+                "pharmacy": None,
+                "rx": None}
+            userinfo = list(data)
+            return render_template('requestpickup.html', userinfo=userinfo)
+
+@app.route('/requestpickup/placeorder', methods=['GET','POST'])
+def placeorder(userinfo=None):
+    if request.method == 'POST':
+        if 'username' in session:
+            userinfo=list(User.populateUserInfo(session["username"]))
+            session["userinfo"]=userinfo
+            #session["order"]=list(User.placeUserOrder(session["username"]))
+
+            return render_template('placeorder.html', userinfo=userinfo)
+        else:
+            userinfo = {"first": request.form['first'],
+                "last": request.form['last'],
+                "phone": request.form['phone'],
+                "dob": request.form['dob'],
+                "address": request.form['address'],
+                "pharmacy": request.form['pharmacy'],
+                "rx": request.form['rx']}
+
+            #store in session so user cannot keep sending post at orderinfo page, order# in session will keep displaying
+            #session["order"]=User.placeorder(userinfo)
+            session["userinfo"]=userinfo
+
+            return render_template('placeorder.html', userinfo=userinfo)
+
+@app.route('/requestpickup/placeorder/orderinfo', methods=['GET','POST'])
+def orderinfo(orderNum=None, fullName=None, phone=None):
+    if request.method == 'POST':
+        if 'username' in session:
+            #need to check input for placeUserOrder() from models
+            orderNum=User.placeUserOrder(session["username"])
+            first = session["userinfo"][0][0]["first"]
+            last = session["userinfo"][0][0]["last"]
+            phone = session["userinfo"][0][0]["phone"]
+            fullName = first + ' ' + last
+            return render_template('orderinfo.html', orderNum=orderNum, fullName=fullName, phone=phone)
+        else:
+            #need to check input for placeorder() from models
+            orderNum=User.placeorder(session["userinfo"])
+            first = session["userinfo"]["first"]
+            last = session["userinfo"]["last"]
+            phone = session["userinfo"]["phone"]
+            fullName = first + ' ' + last
+            return render_template('orderinfo.html', orderNum=orderNum, fullName=fullName, phone=phone)
+
+@app.route('/orderstatus')
+def orderstatus(orderinfo=None):
+    orderinfo = User.checkOrderStatus()
+    return render_template('orderstatus.html', orderinfo=orderinfo)
+
+@app.route('/lookuporder', methods=['GET','POST'])
+def lookuporder(orderinfo=None):
+    if request.method == 'POST':
+        orderinfo = User.lookupOrder(request.form['orderNum'].lower())
+        return render_template('lookuporder.html', orderinfo=orderinfo)
+
+@app.route('/pharmacy')
+def pharmacydashboard(orderinfo=None):
+    orderinfo = Pharmacy.populateOrders()
+    return render_template('pharmacy.html', orderinfo=orderinfo)
+
+@app.route('/pharmacy/fillorder', methods=['GET','POST'])
+def fillorder(orderinfo=None):
+    if request.method == 'POST':
+        Pharmacy.completeOrder(request.form['orderNum'].lower())
+        orderinfo = Pharmacy.populateOrders()
+        return render_template('fillorder.html', orderinfo=orderinfo)
+
+@app.route('/uber')
+def uberdashboard(orderinfo=None):
+    orderinfo = Uber.populateOrders()
+    return render_template('uber.html', orderinfo=orderinfo)
+
+@app.route('/uber/deliver', methods=['GET','POST'])
+def deliver(orderinfo=None):
+    if request.method == 'POST':
+        if request.form['method']=='Start':
+            Uber.startOrder(request.form['orderNum'].lower())
+            orderinfo = Uber.populateOrders()
+            return render_template('deliver.html', orderinfo=orderinfo)
+    else:
+        if request.args.get('method')=='End':
+            Uber.endOrder(request.args.get('orderNum').lower())
+            orderinfo = Uber.populateOrders()
+            return render_template('deliver.html', orderinfo=orderinfo)
+
+@app.route('/user', methods=['GET','POST'])
+def user(orderinfo=None):
+    if request.method == 'POST':
+        orderinfo = User.populateUserOrders(session["username"])
+        return render_template('user.html', orderinfo=orderinfo)
+    elif 'username' in session:
+        orderinfo = User.populateUserOrders(session["username"])
+        return render_template('user.html', orderinfo=orderinfo)
+    else:
+        orderinfo = User.populateOrders()
+        return render_template('user.html', orderinfo=orderinfo)
+
+@app.route('/user/receive', methods=['GET','POST'])
+def receive(orderinfo=None):
+    if request.method == 'POST':
+        if request.form['method']=='Receive':
+            User.receiveOrder(request.form['orderNum'].lower(), request.form['phone'])
+            orderinfo = User.populateOrders()
+            return render_template('receive.html', orderinfo=orderinfo)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if len(username) < 2:
+            flash('Your username must be at least one character.')
+        elif len(password) < 5:
+            flash('Your password must be at least 5 characters.')
+        elif User.find(username):
+            flash('A user with that username already exists.')
+        else:
+            session['credentials'] = {"username": username,
+                "password": password}
+            return redirect(url_for('registration'))
+    return render_template('register.html')
+
+@app.route('/register/registration', methods=['GET','POST'])
+def registration():
+    if request.method == 'POST':
+        userinfo = {"username": session["credentials"]["username"],
+            "password": session["credentials"]["password"],
+            "first": request.form['first'],
+            "last": request.form['last'],
+            "phone": request.form['phone'],
+            "dob": request.form['dob'],
+            "address": request.form['address'],
+            "pharmacy": request.form['pharmacy']
+            }
+        User.register(userinfo)
+        flash('You are now registered. Please login!')
+        return redirect(url_for('login'))
+    return render_template('registration.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if len(list(User.verifyPassword(username, password)))!=0:
+            session["username"]=username
+            return redirect(url_for('user'))
+        else:
+            flash('Invalid username/password! Pls review and reenter!')
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,245 @@
+from py2neo import Graph, Node, Relationship, authenticate
+import uuid
+from datetime import datetime
+#from json import dumps
+import json
+from flask import session
+
+# set up authentication parameters
+authenticate("localhost:7474", "neo4j", "neo4j")
+
+graph = Graph("http://localhost:7474/db/data/")
+
+class User:
+    def placeorder(userinfo):
+        id=str(uuid.uuid4())
+        orderNum=id[-6:]
+
+        user = Node(
+            "User",
+            id=str(uuid.uuid4()),
+            first=userinfo["first"],
+            last=userinfo["last"],
+            phone=userinfo["phone"],
+            dob=userinfo["dob"],
+            address=userinfo["address"],
+            pharmacy=userinfo["pharmacy"],
+            rx=userinfo["rx"]
+        )
+
+        order = Node(
+            "Order",
+            id=str(uuid.uuid4()),
+            orderNum=orderNum,
+            status="New",
+            message="With Pharmacy",
+            date=datetime.now().strftime('%m-%d-%Y')
+        )
+
+        rel = Relationship(user, "PLACED", order)
+
+        graph.create(user)
+        graph.create(order)
+        graph.create(rel)
+
+        return orderNum
+
+    def placeUserOrder(username):
+        id=str(uuid.uuid4())
+        orderNum=id[-6:]
+
+        userinfo = list(User.populateUserInfo(username))
+
+        user = Node(
+            "User",
+            id=str(uuid.uuid4()),
+            first=userinfo[0][0]["first"],
+            last=userinfo[0][0]["last"],
+            phone=userinfo[0][0]["phone"],
+            dob=userinfo[0][0]["dob"],
+            address=userinfo[0][0]["address"],
+            pharmacy=userinfo[0][0]["pharmacy"],
+            rx=userinfo[0][0]["rx"],
+            username=userinfo[0][0]["username"],
+            password=userinfo[0][0]["password"]
+        )
+
+        order = Node(
+            "Order",
+            id=str(uuid.uuid4()),
+            orderNum=orderNum,
+            status="New",
+            message="With Pharmacy",
+            date=datetime.now().strftime('%m-%d-%Y')
+        )
+
+        rel = Relationship(user, "PLACED", order)
+
+        graph.create(user)
+        graph.create(order)
+        graph.create(rel)
+
+        return orderNum
+
+    def timestamp():
+        epoch = datetime.utcfromtimestamp(0)
+        now = datetime.now()
+        delta = now - epoch
+        return delta.total_seconds()
+
+    def checkOrderStatus():
+        query = """
+        MATCH (user:User)-[:PLACED]->(order:Order)
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:FILLED]-(pharmacy:Pharmacy)
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:COMPLETED]-(pharmacy:Pharmacy)
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:PICKEDUP]-(uber:Uber)
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:DELIVERED]-(uber:Uber)
+        OPTIONAL MATCH (user:User)-[:RECEIVED]->(order:Order)<-[:DELIVERED]-(uber:Uber)
+        RETURN order.orderNum as orderNum, order.status as status, order.message as message
+        """
+        return graph.run(query)
+
+    def lookupOrder(orderNum):
+        query = """
+        MATCH (user:User)-[:PLACED]->(order:Order{orderNum:{orderNum}})
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:FILLED]-(pharmacy:Pharmacy)
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:COMPLETED]-(pharmacy:Pharmacy)
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:PICKEDUP]-(uber:Uber)
+        OPTIONAL MATCH (user:User)-[:PLACED]->(order:Order)<-[:DELIVERED]-(uber:Uber)
+        OPTIONAL MATCH (user:User)-[:RECEIVED]->(order:Order)<-[:DELIVERED]-(uber:Uber)
+        RETURN order.orderNum as orderNum, order.status as status, order.message as message
+        """
+        return graph.run(query, orderNum=orderNum)
+
+    def populateOrders():
+        query = """
+        MATCH (user:User)-[:PLACED]->(order:Order)
+        RETURN user, order
+        """
+        return graph.run(query)
+
+    def populateUserOrders(username):
+        query = """
+        MATCH (user:User)-[:PLACED]->(order:Order)
+        WHERE user.username={username}
+        RETURN user, order
+        """
+        return graph.run(query, username=username)
+
+    def populateUserInfo(username):
+        query = """
+        MATCH (user:User)
+        WHERE user.username={username}
+        RETURN user limit 1
+        """
+        return graph.run(query, username=username)
+
+    def receiveOrder(orderNum, phone):
+        query = """
+        MATCH (order:Order), (user:User)
+        WHERE order.orderNum ={orderNum} and user.phone={phone}
+        SET order.status = 'Delivered'
+        SET order.message = 'Package delivered'
+        CREATE (user)-[:RECEIVED]->(order)
+        """
+        return graph.run(query, orderNum=orderNum, phone=phone)
+
+    def find(username):
+        user = graph.find_one("User", "username", username)
+        return user
+
+    def register(userinfo):
+        user = Node(
+            "User",
+            id=str(uuid.uuid4()),
+            username=userinfo["username"],
+            password=userinfo["password"],
+            first=userinfo["first"],
+            last=userinfo["last"],
+            phone=userinfo["phone"],
+            dob=userinfo["dob"],
+            address=userinfo["address"],
+            pharmacy=userinfo["pharmacy"]
+        )
+        graph.create(user)
+
+    def verifyPassword(username, password):
+        query = """
+        MATCH (user:User)
+        WHERE user.username={username} and user.password={password}
+        RETURN user.username as username limit 1
+        """
+        return graph.run(query, username=username, password=password)
+
+class Pharmacy:
+    def populateOrders():
+        query = """
+        MATCH (user:User)-[:PLACED]->(order:Order)
+        RETURN user, order
+        """
+        return graph.run(query)
+
+    def completeOrder(orderNum):
+        pharmacy = Node(
+            "Pharmacy",
+            name="Pharmacy1"
+        )
+        graph.create(pharmacy)
+
+        query = """
+        MATCH (order:Order), (pharmacy:Pharmacy)
+        WHERE order.orderNum ={orderNum} and pharmacy.name='Pharmacy1'
+        SET order.status = 'Ready'
+        SET order.message = 'Requesting ride'
+        CREATE (pharmacy)-[:COMPLETED]->(order)
+        """
+        return graph.run(query, orderNum=orderNum)
+
+    def register(userinfo):
+        user = Node(
+            "User",
+            id=str(uuid.uuid4()),
+            first=userinfo["first"],
+            last=userinfo["last"],
+            phone=userinfo["phone"],
+            dob=userinfo["dob"],
+            address=userinfo["address"],
+            pharmacy=userinfo["pharmacy"],
+            rx=userinfo["rx"]
+        )
+        graph.create(user)
+
+class Uber:
+    def populateOrders():
+        query = """
+        MATCH (user:User)-[:PLACED]->(order:Order)
+        RETURN user, order
+        """
+        return graph.run(query)
+
+    def startOrder(orderNum):
+        uber = Node(
+            "Uber",
+            name="Uber1",
+            rate="****"
+        )
+        graph.create(uber)
+
+        query = """
+        MATCH (order:Order), (uber:Uber)
+        WHERE order.orderNum ={orderNum} and uber.name='Uber1'
+        SET order.status = 'Enrouted'
+        SET order.message = 'Ride starts'
+        CREATE (uber)-[:STARTED]->(order)
+        """
+        return graph.run(query, orderNum=orderNum)
+
+    def endOrder(orderNum):
+        query = """
+        MATCH (order:Order), (uber:Uber)
+        WHERE order.orderNum ={orderNum} and uber.name='Uber1'
+        SET order.status = 'Arrived'
+        SET order.message = 'Arrived at destination'
+        CREATE (uber)-[:ENDED]->(order)
+        """
+        return graph.run(query, orderNum=orderNum)
